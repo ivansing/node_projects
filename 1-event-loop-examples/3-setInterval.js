d2ffd8449e03ed45a47d446b13e3d45ce006ae55
ivansing <lancedealway@gmail.com>
2023-09-25 16:22:39 -0500

    upload files content

---
+++ 1-event-loop-examples/3-setInterval.js
@@ -0,0 +1,8 @@
+// set time to repeat event everytime
+setInterval(()=>{
+  console.log('hello world')
+},2000)
+console.log(`I will run first`)
+// process stays alive unless
+// Kill Process CONTROL + C
+// unexpected error
---
+++ 14-request-event.js
@@ -0,0 +1,13 @@
+const http = require('http')
+
+
+// Using Event Emitter API
+const server = http.createServer()
+
+// emits request event
+// subcribe to it / listen for it / respond for it
+server.on('request', (req,res)=>{
+  res.end('Welcome')
+})
+
+server.listen(5000)
